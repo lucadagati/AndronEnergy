d52cbd56a1f3ee42432276a7fabf9f392c6fac3c
MIKE9708 <paonelucio97@gmail.com>
2022-09-21 22:00:57 +0200

    chaincode update

--- chaincode/src/subcontract/pod/podStruct.ts
+++ chaincode/src/subcontract/pod/podStruct.ts
@@ -8,19 +8,25 @@ export class PodStruct{
     @Property()
     public podId:string;
     @Property()
-    public plantIds:string[]
+    public comunityId:string;
+    @Property()
+    public plantIds:string[];
+    @Property()
+    public userConsumptionIds:string[];
     @Property()
     public exchangedEnergy:{"time":number,"exchangedEnergy":number}[];
     @Property()
     public storedEnergy:{"time":number,"storedEnergy":number}[];
     @Property()
     public  offgrid:string;
-    constructor(podId:string,plantIds:string[],exchangedEnergy:{"time":number,"exchangedEnergy":number}[],storedEnergy:{"time":number,"storedEnergy":number}[],offgrid:string){
+    constructor(podId:string,userConsumptionId:string[],plantIds:string[],exchangedEnergy:{"time":number,"exchangedEnergy":number}[],storedEnergy:{"time":number,"storedEnergy":number}[],offgrid:string,comunityId:string){
         this.podId=podId;
         this.plantIds=plantIds;
         this.offgrid=offgrid;
         this.exchangedEnergy=exchangedEnergy;
         this.storedEnergy=storedEnergy;
+        this.comunityId=comunityId;
+        this.userConsumptionIds=userConsumptionId;
     }
 
 }
--- chaincode/src/subcontract/podPlant/plantStruct.ts
+++ chaincode/src/subcontract/podPlant/plantStruct.ts
@@ -3,15 +3,18 @@ import {Object,Property} from 'fabric-contract-api';
 @Object()
 export class PlantStruct{
     @Property()
-    public readonly type: string = "plant"
+    public readonly type: string = "plant";
+    @Property()
+    public readonly podId: string;
     @Property()
     public plantId:string;
 
     @Property()
     public generatedEnergy:{"time":number,"consumption":number}[];
     
-    constructor(plantId:string,generatedEnergy:{"time":number,"consumption":number}[]){
+    constructor(plantId:string,podId:string,generatedEnergy:{"time":number,"consumption":number}[]){
         this.plantId=plantId;
+        this.podId=podId;
         this.generatedEnergy=generatedEnergy;
     }
 }
